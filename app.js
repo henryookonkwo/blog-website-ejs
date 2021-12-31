47f0263216eb57985288d3f567a62c103df815c7
System Administrator <root@Henrys-MacBook-Pro.local>
2021-12-31 14:30:31 -0500

    add more functionalities

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,13 +11,22 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req,res) => {
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {startingContent: homeStartingContent, 
+    // postTitle:posts[0].title,
+    // postContent: posts[0].content 
+    posts:posts
+  });
+
+  // console.log(posts[0].content);
 });
 
 app.get("/about", (req,res) => {
@@ -31,14 +41,38 @@ app.get("/compose", (req,res) => {
   res.render("compose");
 });
 
+app.post("/compose", (req, res) => {
+  // console.log(req.body);
+  // let composePostTitle = req.body.postTitle;
+  // let composePostBody = req.body.postBody;
+  const post = {
+    title: req.body.postTitle, 
+    content: req.body.postBody
+  };
+  posts.push(post);
 
+  res.redirect("/");
+  // console.log(post);
+  
+})
 
+app.get("/posts/:postName", (req,res) => {
+  // console.log(req.params.postName);
+  const requestedTitle = _.lowerCase(req.params.postName);
+  posts.forEach((post) => {
+    const storedTitle = _.lowerCase(post.title);
+    if (requestedTitle === storedTitle){
+      // console.log("match found");
+      res.render("post", {
+        postTitle: post.title,
+        postContent: post.content 
+      });
+    } 
 
+  });
 
-
-
-
-
+  
+});
 
 
 
